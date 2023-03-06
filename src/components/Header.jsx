a8a58e47d3464aaf284b09491775559994d44a17
Nazmul Haque <bpinazmul18@gmail.com>
2023-03-06 08:36:59 +0600

    ADD: Working on Header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
 
 function Header() {
-  return <header>Header</header>
+  return (
+    <header>
+      <input
+        type="checkbox"
+        name="hbr"
+        id="hbr"
+        className="hbr peer"
+        hidden
+        aria-hidden="true"
+      />
+      <nav className="fixed z-20 w-full bg-white/80 dark:bg-gray-900/80 backdrop-blur navbar shadow-md shadow-gray-600/5 peer-checked:navbar-active md:relative md:bg-transparent dark:shadow-none">
+        <div className="xl:container m-auto px-6 md:px-12">
+          <div className="flex flex-wrap items-center justify-between gap-6 md:py-3 md:gap-0">
+            <div className="w-full flex justify-between lg:w-auto">
+              <a
+                href="!#"
+                aria-label="logo"
+                className="flex space-x-2 items-center"
+              >
+                <div aria-hidden="true" className="flex space-x-1">
+                  <div className="h-4 w-4 rounded-full bg-gray-900 dark:bg-gray-200" />
+                  <div className="h-6 w-2 bg-primary dark:bg-primaryLight" />
+                </div>
+                <span className="text-base font-bold text-gray-600 dark:text-white">
+                  SASS
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
